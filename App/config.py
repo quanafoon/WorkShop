0674b680d8d9540a0f4756b6b4c14497cdbe7bb1
quanafoon <126298411+quanafoon@users.noreply.github.com>
2024-04-14 09:10:31 +0000

    import

--- App/config.py
+++ App/config.py
@@ -1,6 +1,13 @@
 import os
 
 def load_config(app, overrides):
+    config = {'ENV': os.environ.get('ENV', 'DEVELOPMENT')}
+    delta = 7
+    if config['ENV'] == "DEVELOPMENT":
+        from .default_config import JWT_ACCESS_TOKEN_EXPIRES, SQLALCHEMY_DATABASE_URI, SECRET_KEY
+        config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+        config['SECRET_KEY'] = SECRET_KEY
+        delta = JWT_ACCESS_TOKEN_EXPIRES
     if os.path.exists(os.path.join('./App', 'custom_config.py')):
         app.config.from_object('App.custom_config')
     else:
