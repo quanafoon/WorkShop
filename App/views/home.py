da33b1a5c8cdb611911eeedc4e392586f4945b77
Quan Afoon <quanafoon@gmail.com>
2024-04-21 22:29:49 +0000

    change design

--- App/views/home.py
+++ App/views/home.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for, flash
-from App.controllers import create_user, parse_internships, apply, getInternships, findInternship
+from App.controllers import create_user, parse_internships, apply, getInternships, findInternship, get_user_by_username, verify
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 import os
 from werkzeug.utils import secure_filename
@@ -8,20 +8,19 @@ from werkzeug.utils import secure_filename
 home_views = Blueprint('home_views', __name__, template_folder='../templates')
 
 @home_views.route('/home', methods=['GET'])
-@jwt_required()
 def home_page():
     internships = getInternships()
     return render_template('home.html', internships=internships)
 
 @home_views.route('/home/<int:id>', methods=['GET'])
-@jwt_required()
 def selectInternship(id):
     selected = findInternship(id)
     return render_template('home.html', selected=selected)
 
 @home_views.route('/apply/<int:id>', methods=['POST'])
-@jwt_required()
 def apply_action(id):
+    username = request.form.get('username')
+    password = request.form.get('password')
     firstname = request.form.get('firstname')
     lastname = request.form.get('lastname')
     dob = request.form.get('dob')
@@ -29,7 +28,14 @@ def apply_action(id):
     phone = request.form.get('phone')
     transcript = request.files['transcript']
     resume = request.files['resume']
-    user_id = jwt_current_user
+    user = verify(username, password)
+    if user:
+        user_id = user.id
+    else:
+        flash("Invalid username/passowrd")
+        return redirect(url_for('home_views.apply_action', id=id))
+
+
     upload_folder = 'uploads'
     os.makedirs(upload_folder, exist_ok=True)
 
