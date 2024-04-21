d89b7fe50967e47c3e4a8931aa4d9d8f2468c08e
Quan Afoon <quanafoon@gmail.com>
2024-04-21 22:44:21 +0000

    cleanup

--- App/views/home.py
+++ App/views/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for, flash
-from App.controllers import create_user, parse_internships, apply, getInternships, findInternship, get_user_by_username, verify
-from flask_jwt_extended import jwt_required, current_user as jwt_current_user
+from App.controllers import create_user, parse_internships, apply, getInternships, findInternship
+from flask_jwt_extended import jwt_required, current_user
 import os
 from werkzeug.utils import secure_filename
 
@@ -18,9 +18,8 @@ def selectInternship(id):
     return render_template('home.html', selected=selected)
 
 @home_views.route('/apply/<int:id>', methods=['POST'])
+@jwt_required()
 def apply_action(id):
-    username = request.form.get('username')
-    password = request.form.get('password')
     firstname = request.form.get('firstname')
     lastname = request.form.get('lastname')
     dob = request.form.get('dob')
@@ -28,13 +27,7 @@ def apply_action(id):
     phone = request.form.get('phone')
     transcript = request.files['transcript']
     resume = request.files['resume']
-    user = verify(username, password)
-    if user:
-        user_id = user.id
-    else:
-        flash("Invalid username/passowrd")
-        return redirect(url_for('home_views.apply_action', id=id))
-
+    user_id = current_user.id
 
     upload_folder = 'uploads'
     os.makedirs(upload_folder, exist_ok=True)
